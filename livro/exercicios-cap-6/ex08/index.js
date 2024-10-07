9a70cf90b56f30b22206ff049eb9949429fee5ef
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-10-06 23:09:52 -0300

    Exercicio 08 do cap 6 finalizado

--- livro/exercicios-cap-6/ex08/index.js
+++ livro/exercicios-cap-6/ex08/index.js
@@ -17,12 +17,39 @@ frm.addEventListener('submit', (e) => {
         vetNumeros.push(numero);
     }
 
-    let lista = 'Lista de numeros:\n';
-    lista += `${vetNumeros.join(", ")}`;
-
-    resp.innerText = lista;
-
     frm.reset();
     frm.inNumero.focus();
+
+});
+
+frm.btVerificarOrdem.addEventListener('click', (e) => {
+    const vetCopia = [...vetNumeros];
+    let contador = 0;
+    let lista = `Numeros: ${vetNumeros}\n`;
+
+    vetCopia.sort((a, b) => a - b);
+
+    for(let i = 0; i < vetNumeros.length; i++){
+        if(vetNumeros[i] === vetCopia[i]){
+            contador += 1
+        }
+    }
+
+    if (contador === vetNumeros.length) {
+        resp.innerText = lista;
+        resp2.innerText = `Atenção... Os números estão em ordem crescente`;
+    
+        // Remover classe anterior e adicionar a classe correspondente
+        resp2.classList.remove('ordem-nao-crescente');
+        resp2.classList.add('ordem-crescente');
+    } else {
+        resp.innerText = lista;
+        resp2.innerText = `Atenção... Os números não estão em ordem crescente`;
+    
+        // Remover classe anterior e adicionar a classe correspondente
+        resp2.classList.remove('ordem-crescente');
+        resp2.classList.add('ordem-nao-crescente');
+    }
+    
 
 });
