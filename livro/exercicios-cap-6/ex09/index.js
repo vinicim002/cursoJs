5644c74ee152c2986b6b5213e2a3106a291658b6
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-10-09 22:13:36 -0300

    ex 09 do cap 6 finalizado

--- livro/exercicios-cap-6/ex09/index.js
+++ livro/exercicios-cap-6/ex09/index.js
@@ -1,8 +1,6 @@
 const frm = document.querySelector("form");
 const resp = document.querySelector('pre');
 
-const minAprovacao = Number(prompt("Numero de acertos para aprovacao:"));
-
 const vetCandidato = [];
 
 frm.addEventListener('submit', (e) => {
@@ -13,10 +11,19 @@ frm.addEventListener('submit', (e) => {
 
     vetCandidato.push({nomeCandidato, numeroAcertos});
 
+    let lista = "";
+
+    for(const candidatos of vetCandidato){
+        const {nomeCandidato, numeroAcertos} = candidatos;
+        // const {nomeCandidato, numeroAcertos} = candidatos:
+        // Isso é uma desestruturação de objeto. Significa que a cada iteração, o objeto candidatos tem suas propriedades extraídas e armazenadas nas variáveis nomeCandidato e numeroAcertos. Isso pressupõe que o objeto candidatos tem as propriedades nomeCandidato (nome do candidato) e numeroAcertos (quantidade de acertos desse candidato).
+        lista += `${nomeCandidato} - ${numeroAcertos} acertos\n`; 
+    }
+    
+    resp.innerText = lista;
+
     frm.reset();
     frm.inCandidatos.focus();
-
-    frm.btListar.dispatchEvent(new Event('click'));
 });
 
 frm.btListar.addEventListener('click', () => {
@@ -25,6 +32,47 @@ frm.btListar.addEventListener('click', () => {
         return;
     }
 
+    let lista = "";
     
+    for(const candidatos of vetCandidato){
+        const {nomeCandidato, numeroAcertos} = candidatos;
+        lista += `${nomeCandidato} - ${numeroAcertos} acertos\n`;
+    }
+
+    resp.innerText = lista;
 })
+
+frm.btAprovados.addEventListener('click', () => {
+    if(vetCandidato.length == 0){
+        alert("Nao existe candidatos cadastrados");
+        return;
+    }
+
+    const minAprovacao = Number(prompt("Numero de acertos para aprovacao:"));
+
+    if (minAprovacao == 0 || isNaN(minAprovacao)){
+        alert("Número inválido");
+        return;
+    }
+
+    const copia = [...vetCandidato];
+
+    copia.sort((a, b) => {a.numeroAcertos - b.numeroAcertos});
+    copia.reverse();
+
+    let aprovados = "";
+
+    for(const candidatos of copia){
+        if(candidatos.numeroAcertos >= minAprovacao){
+            aprovados += `${candidatos.nomeCandidato} - ${candidatos.numeroAcertos} acertos\n`
+        }
+    }
+
+
+    if(aprovados == ""){
+        resp.innerText = "Nao ha aprovados!";
+    } else {
+        resp.innerText = aprovados;
+    }
+});
 
