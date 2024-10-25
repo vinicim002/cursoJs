0941f3a5b93008872aebf627b3fa78e592ecffbb
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-10-25 00:12:55 -0300

    funcao Construtora

---
+++ secao 006/014-2funcaoConstrutora.js
@@ -0,0 +1,14 @@
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+};
+
+
+let pessoa1 = new Pessoa("Joao", 32);
+let pessoa2 = new Pessoa("Maria", 28);
+
+console.log(pessoa1);
+console.log(pessoa2);
+
