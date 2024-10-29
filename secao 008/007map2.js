575469b0095a412ec1699b6e810567a7acbf8be8
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-10-29 11:50:13 -0300

    Algumas funcionalidades do array

---
+++ secao 008/007map2.js
@@ -0,0 +1,11 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45}',
+    '{ "nome": "Caderno", "preco": 13.90}',
+    '{ "nome": "Caneta", "preco": 7.50}'
+]
+
+const paraObjeto = json => JSON.parse(json);
+const apenasPreco = produto => produto.preco;
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco);
+console.log(resultado);
