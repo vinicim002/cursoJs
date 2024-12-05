d7897320ca9353b82182878236641ffb48c04fb5
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-12-04 22:04:48 -0300

    Voltar dps, pois n deu certo

--- livro/exercicios-cap-7/ex08/index.js
+++ livro/exercicios-cap-7/ex08/index.js
@@ -1,14 +1,26 @@
-const frm = document.querySelector('form'); //Obtendo conteudo
-const resp = document.querySelector('pre');
+const frm = document.querySelector('form');
+const resp = document.querySelector('#resp1');
+const resp2 = document.querySelector('#resp2');
 
 frm.addEventListener('submit', (e) => {
-    e.preventDefault(); //Evitando envio do forms
+  e.preventDefault();
 
-    const dataInfracao = frm.inData.value;
-    const valorMulta = frm.inValor.value;
-    const hoje = new Date();
-    const venc = new Date();
+  const dataInfracao = frm.inData.value;
+  const valorMulta = parseFloat(frm.inValor.value);
 
+  // Criar uma nova data a partir da data de infração
+  const partes = dataInfracao.split("-");
+  const dataVenc = new Date(partes[0], partes[1] - 1, partes[2]);
 
+  // Adicionar 90 dias à data de vencimento
+  dataVenc.setDate(dataVenc.getDate() + 90);
+
+  const dia = dataVenc.getDate();
+  const mes = dataVenc.getMonth() + 1;
+  const ano = dataVenc.getFullYear();
+
+  const comDesconto = valorMulta * 0.80;
+
+  resp.innerText = "Data Limite para Pagto com Desconto: " + (dia < 10 ? "0" + dia : dia) + "/" + (mes < 10 ? "0" + mes : mes) + "/" + ano;
+  resp2.innerText = "Valor com Desconto R$: " + comDesconto.toFixed(2);
 });
-
