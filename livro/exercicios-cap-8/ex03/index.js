5111ee11b85c1d5b55bafae0b762ec33f84114cc
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-02-10 17:40:33 -0300

    .

---
+++ livro/exercicios-cap-8/ex03/index.js
@@ -0,0 +1,47 @@
+const frm = document.querySelector('form');
+const resp = document.querySelector('pre');
+
+const validarNome = (nome) => {
+    let validacao = true;
+
+    if(nome === ""){ //Verifico se a minha string n esta vazia
+        validacao = false;
+    };
+
+    if(nome.trim() === ""){ //Verifico se existe algum espaco antes ou no fim
+        validacao = false; 
+    };
+
+    if(nome.length < 3 || nome.length > 50){ //Declaro q minha string tem q ter pelo menos 3 caracteres e no maximo 50
+        validacao = false;
+    };
+
+    if(!/^[a-zA-Z\s]+$/.test(nome)) { //Faco uma regex para dizer que o input aceita somente letras
+        validacao = false;
+    };
+
+    const partes = nome.split(" "); //Vejo quantos parte tenha a minha string
+
+    if(partes.length < 2) { //Delcaro q ela n poder ser menor q duas partes 
+        validacao = false;
+    };
+
+    return validacao;
+};
+
+const obterSobrenome = (nome) => {
+    const sobrenome = nome.split(" "); //Vejo quantos parte tenha a minha string
+
+    return sobrenome[sobrenome.length - 1]; //Declaro que qro obter a ultima parte da minha string
+};
+
+const contarVogais = (nome) => {
+
+};
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nome = frm.inNome.value;
+})
