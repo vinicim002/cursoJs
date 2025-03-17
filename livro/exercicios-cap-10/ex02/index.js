f31cb3005b9919488070df52ef411566b8c07d9e
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-03-17 17:27:13 -0300

    Iniciando exA do cap 10

---
+++ livro/exercicios-cap-10/ex02/index.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form");
+const dvformContainner = document.querySelector("#form-containner");
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const idade = frm.inIdade.value;
+});
