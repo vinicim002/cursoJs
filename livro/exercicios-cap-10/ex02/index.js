3190cec574cde47ace4f488eeb9dfbc2e9a7aff6
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-03-17 23:41:23 -0300

    Exercicio a do cap 10 resolvido

--- livro/exercicios-cap-10/ex02/index.js
+++ livro/exercicios-cap-10/ex02/index.js
@@ -5,5 +5,21 @@ const dvformContainner = document.querySelector("#form-containner");
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const idade = frm.inIdade.value;
+    const idade = Number(frm.inIdade.value);
+
+    const strIdade = idade.toString();
+
+    for (const num of strIdade) {
+        const img = document.createElement("img")
+        img.src = `img/${num}.jpg`
+        img.alt = `Vela de ${num} anos`
+        dvformContainner.appendChild(img)
+      }
+    
+      btExibir.disabled = true
+
 });
+
+frm.addEventListener("reset", () => {
+    location.reload()
+  })
