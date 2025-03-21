83065c1e8359cc89cc63a381e616413d297ec76a
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-03-20 22:24:37 -0300

    +1 exercicio

--- Exercicios/ex11.js
+++ Exercicios/ex11.js
@@ -1,11 +1,15 @@
 const removerPropiedade = (obj, nomePropiedade) => {
-    delete obj[nomePropiedade]; // Modifica diretamente o objeto original
-    return obj; // Retorna o próprio objeto modificado
+    const copyObj = { ...obj }; // Cópia do objeto
+
+    delete copyObj[nomePropiedade]; // Remove a propriedade desejada
+    
+    return copyObj; // Retorna a cópia modificada
 };
 
-const objeto = { id: 20, nome: "caneta", descricao: "Nao preenchido" };
+console.log(removerPropiedade({a: 1, b: 2}, "a"));
+console.log(removerPropiedade({
+    id: 20,
+    nome: "caneta",
+    descricao: "Nao preenchido"
+}, "descricao"));
 
-console.log(removerPropiedade(objeto, "descricao")); // Saída: { id: 20, nome: "caneta" }
-
-// Comparando o objeto original com o resultado da remoção
-console.log(Object.is(objeto, removerPropiedade(objeto, "descricao"))); // Saída: true
