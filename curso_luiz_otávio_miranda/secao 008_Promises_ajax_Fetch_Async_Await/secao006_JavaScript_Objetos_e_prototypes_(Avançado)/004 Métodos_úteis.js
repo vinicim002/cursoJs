0a6e6617590abe72fad5221da6157df7e91ab78d
Vinicius Vila Nova <viniciusvilanova7@gmail.com>
2025-05-28 18:02:13 -0300

    aula de metodos uteis finalizada

--- curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao006_JavaScript_Objetos_e_prototypes_(Avançado)/004 Métodos_úteis.js
+++ curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao006_JavaScript_Objetos_e_prototypes_(Avançado)/004 Métodos_úteis.js
@@ -23,10 +23,24 @@ Object.defineProperty (define uma propiedade)
 // console.log(caneca);
 
 const produto = { nome: "Caenca", preco: 1.8 };
-//Copiar um objeto
-const caneca = Object.assign({}, produto, { material: 'porcelana'});
 
-caneca.nome = "Outro nome";
-caneca.preco = 2.5;
-console.log(produto);
-console.log(caneca);
+//Mostra array com chaves e valores
+console.log(Object.entries(produto));
+
+//Pegando so os valores
+// console.log(Object.values(produto));
+
+//Descreve as propriedades e sua configs
+// console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+
+//Congela o objeto com isso nao consigo alterar mais nada dele
+// Object.freeze(produto);
+// produto.nome = "Escova"; // nao eh atribuido devido ao freeze
+// console.log(produto.nome);
+
+//Copiar um objeto
+// const caneca = Object.assign({}, produto, { material: 'porcelana'});
+
+//Mostrando as chaves do objeto
+// console.log(Object.keys(produto));
+
