875fc6f1942429fface8501ab0fb4bca1b87abaa
Vinicius Vila Nova <viniciusvilanova7@gmail.com>
2025-05-31 21:56:35 -0300

    Aula 6 finalizada

--- curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao006_JavaScript_Objetos_e_prototypes_(Avançado)/006 manipulando_prototypes.js
+++ curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao006_JavaScript_Objetos_e_prototypes_(Avançado)/006 manipulando_prototypes.js
@@ -1,15 +1,42 @@
 // new Object -> Object.prototype
 
-const objA = {
-    chaveA: 'A'
-};
+// const objA = {
+//     chaveA: 'A'
+// };
 
 // new Object -> Object.prototype
 
-const objB = {
-    chaveB: 'B'
-};
+// const objB = {
+//     chaveB: 'B'
+// };
 
-Object.setPrototypeOf(objB, objA);
+// Object.setPrototypeOf(objB, objA);
 
-console.log(objB.chaveA);
+// console.log(objB.chaveA);
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+}
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+const p1 = new Produto('Camiseta', 50);
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+}
+
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+
+console.log(p1);
+console.log(p2); 
