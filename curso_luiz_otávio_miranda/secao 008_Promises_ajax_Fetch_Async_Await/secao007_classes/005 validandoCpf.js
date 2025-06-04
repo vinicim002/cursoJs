13c648783ec89d837f6d6813af5df735ee629348
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-06-03 22:42:15 -0300

    Exercicio valida cpf finalizado

--- curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao007_classes/005 validandoCpf.js
+++ curso_luiz_otávio_miranda/secao 008_Promises_ajax_Fetch_Async_Await/secao007_classes/005 validandoCpf.js
@@ -24,9 +24,37 @@ class ValidaCpf {
         return this.cpfEnviado.replace(/\D+/g, '');
     }
 
+    isSequencia() {
+        return this.cpfLimpo.split('').every(char => char === this.cpfLimpo[0]);
+    }
+
     valida() {
-        if(typeof this.cpfLimpo === 'undefined') return false;
-        if(this.cpfLimpo.length !== 11 ) return false;
-        if(this.isSequencia()) return false;
+        if (typeof this.cpfLimpo === 'undefined') return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (this.isSequencia()) return false;
+
+        const cpfParcial = Array.from(this.cpfLimpo).map(Number).slice(0, 9);
+        const digito1 = this.criaDigito(cpfParcial);
+        const digito2 = this.criaDigito([...cpfParcial, digito1]);
+
+        const novoCpf = [...cpfParcial, digito1, digito2].join('');
+        return novoCpf === this.cpfLimpo;
+    }
+
+    criaDigito(cpfParcial) {
+        const pesoInicial = cpfParcial.length + 1;
+        const soma = cpfParcial.reduce((total, num, indice) => {
+            return total + num * (pesoInicial - indice);
+        }, 0);
+
+        const digito = 11 - (soma % 11);
+        return digito > 9 ? 0 : digito;
     }
 }
+
+const cpf1 = new ValidaCpf('705.484.450-52');
+console.log(cpf1.valida()); // true
+
+const cpf2 = new ValidaCpf('123.456.789-00');
+console.log(cpf2.valida()); // false
+
