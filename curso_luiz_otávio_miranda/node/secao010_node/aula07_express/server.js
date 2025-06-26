01c9e1a60dfb0077d5fbcf2961b59a8c20f283d0
Vinicius Vila Nova <viniciusvilanova7@gmail.com>
2025-06-25 21:51:03 -0300

    express

---
+++ curso_luiz_otávio_miranda/node/secao010_node/aula07_express/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+//CRUD => CREATE, READ, UPDATE, DELETE
+//        POST    GET   PUT     DELETE
+
+//Rotas
+//https://meusite.com/ <= get => Entrega a pagina / (home)
+//https://meusite.com/sobre <= get => Entrega a pagina /sobre
+//https://meusite.com/contato <= get => Entrega a pagina /contato
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </form>
+        `);
+});
+
+app.post('/', (req, res) => {
+    res.send("Recebi o formulario!")
+})
+
+app.get('/contato', (req, res) => {
+    res.send("Obrigado por entrar em contato com a gente");
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log(`Servidor executando na porta 3000`);
+});
+
