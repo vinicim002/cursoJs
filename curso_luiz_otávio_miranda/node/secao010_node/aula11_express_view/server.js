66885c6297c235a4c8f3fa161384697069a73973
Vinicius <104523463+vinicim002@users.noreply.github.com>
2025-06-26 11:00:23 -0300

    apresentacao da views

--- curso_luiz_otávio_miranda/node/secao010_node/aula11_express_view/server.js
+++ curso_luiz_otávio_miranda/node/secao010_node/aula11_express_view/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path =require('path');
 
 app.use(express.urlencoded({ extended: true}));
+
+//Buscando onde o meu view esta localizado, usando o caminho absoluto
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
 app.use(routes);
 
 app.listen(3000, () => {
