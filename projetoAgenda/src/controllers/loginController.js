312c5e53e9e59d656f1ec22c290b72f1cc5bdb5e
Vinicius Vila Nova <viniciusvilanova7@gmail.com>
2025-08-15 17:23:25 -0300

    login e register cadastrado

--- projetoAgenda/src/controllers/loginController.js
+++ projetoAgenda/src/controllers/loginController.js
@@ -1,7 +1,27 @@
 const Login = require("../models/LoginModel");
 
 exports.index = (req, res) => {
-  res.status(200).render("login");
+  if (req.session.user) return res.render('login_logado');
+  return res.status(200).render("login");
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      return req.session.save(() => res.redirect("/login/index"));
+    }
+
+    req.flash("success", "Voce entrou no sistema");
+    req.session.user = login.user;
+    return req.session.save(() => res.redirect("/login/index"));
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
 };
 
 exports.register = async (req, res) => {
@@ -16,10 +36,14 @@ exports.register = async (req, res) => {
 
     req.flash("success", "Seu usuário foi criado com sucesso!");
     return req.session.save(() => res.redirect("/login/index"));
-
   } catch (e) {
     console.log(e);
     return res.render("404");
   }
 };
 
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
+}
--- projetoAgenda/src/models/LoginModel.js
+++ projetoAgenda/src/models/LoginModel.js
@@ -1,70 +1,84 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
 
 //Modelagem do dados
 const LoginSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-const LoginModel = mongoose.model('Login', LoginSchema);
+const LoginModel = mongoose.model("Login", LoginSchema);
 
 class Login {
-    constructor(body) {
-        this.body = body;
-        this.errors = [];
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
+
+  async login() {
+    this.valida();
+    if (this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user) {
+        this.errors.push("Usuario nao existe");
+        return;
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+        this.errors.push("Senha invalida");
         this.user = null;
+        return
+    }
+  }
+
+  async register() {
+    this.valida();
+    if (this.errors.length > 0) return;
+
+    await this.userExists();
+
+    if (this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    this.user = await LoginModel.create(this.body);
+  }
+
+  async userExists() {
+    try {
+      this.user = await LoginModel.findOne({ email: this.body.email });
+      if (this.user) this.errors.push("Usuario ja existe");
+    } catch (e) {
+      console.log();
+    }
+  }
+
+  valida() {
+    this.cleanUp();
+
+    if (!validator.isEmail(this.body.email))
+      this.errors.push("E-mail invalido!");
+    if (this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push("A senha precisa ter entre 3 e 50 caracteres!");
+    }
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== "string") {
+        this.body[key] = "";
+      }
     }
 
-    async register() {
-        this.valida();
-        if(this.errors.length > 0) return;
-
-        await this.userExists();
-
-        if(this.errors.length > 0) return;
-
-        const salt = bcrypt.genSaltSync();
-        this.body.password = bcrypt.hashSync(this.body.password, salt);
-
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
-        }   
-    }
-
-    async userExists() {
-        try {
-            const user = await LoginModel.findOne( {email: this.body.email });
-            if (user) this.errors.push('Usuario ja existe');
-        } catch (e) {
-            console.log();
-        }
-    }
-
-    valida() {
-        this.cleanUp();
-
-        if(!validator.isEmail(this.body.email)) this.errors.push("E-mail invalido!");
-        if(this.body.password.length < 3 || this.body.password.length > 50) {
-            this.errors.push("A senha precisa ter entre 3 e 50 caracteres!");
-        }
-    }
-
-    cleanUp() {
-        for(const key in this.body) {
-            if(typeof this.body[key] !== 'string') {
-                this.body[key] = '';
-            }
-        }
-
-        this.body = {
-            email: this.body.email,
-            password: this.body.password
-        }
-    }
+    this.body = {
+      email: this.body.email,
+      password: this.body.password,
+    };
+  }
 }
 
 module.exports = Login;
