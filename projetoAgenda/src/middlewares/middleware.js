581dcafba43ea6c17b81dc74064b0f57fbe99265
Vinicius Vila Nova <viniciusvilanova7@gmail.com>
2025-08-15 09:42:51 -0300

    designer de login finalizado

--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -4,9 +4,11 @@ exports.middlewareGlobal = (req, res, next) => {
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code ) {
+    if(err) {
         return res.render('404');
     }
+
+    next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
